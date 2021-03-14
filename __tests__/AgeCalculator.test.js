4aba0e76459f38c326d8a3557ab34dc485469305
Saoud Rana <saoud@saoud.dev>
2021-03-13 16:16:38 -0800

    changing test file name and adding in first test with a bad fail currently though

---
+++ __tests__/AgeCalculator.test.js
@@ -0,0 +1,15 @@
+// import { TestScheduler } from 'jest';
+import AgeCalculator from '../src/galacticAge.js'
+
+describe('AgeCalculator', () => {
+
+  let AgeCalculator; 
+
+  beforeEach(() => {
+    AgeCalculator = new AgeCalculator(30, lifeExpectancy)
+    AgeCalculator.age = 30;
+  });
+  test('It should have 30 stored in the age property', () => {
+    expect (AgeCalculator.age).toEqual(30);
+  });
+});
