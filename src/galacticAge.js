67ce1c0fc262741a114ede0c4414bb3bef921009
Saoud <saoud@saoud.dev>
2021-03-19 14:50:54 -0700

    round to one decimal place

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -2,10 +2,10 @@ export default class AgeCalculator {
   constructor (age, lifeExpectancy, mercuryAge, marsAge, venusAge, jupiterAge, yearsLeft) {
   this.age = age;
   this.lifeExpectancy = lifeExpectancy;
-  this.mercuryAge = .24;
-  this.venusAge = .62;
-  this.marsAge = 1.88;
-  this.jupiterAge = 11.86;
-  this.yearsLeft = 90 - age;
+  this.mercuryAge = parseFloat(age * .24).toFixed(1);
+  this.venusAge = parseFloat(age * .62).toFixed(1);
+  this.marsAge = parseFloat(age * 1.88).toFixed(1);
+  this.jupiterAge = parseFloat(age * 11.86).toFixed(1);
+  this.yearsLeft = parseFloat(90 - age).toFixed(1);
   }
 }
