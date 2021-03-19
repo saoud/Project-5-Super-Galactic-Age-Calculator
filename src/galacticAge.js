81a6da247fbb737120e6366f3ed184ef30ed7bcb
Saoud <saoud@saoud.dev>
2021-03-19 14:41:21 -0700

    add the outline of the first constructor

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -2,9 +2,10 @@ export default class AgeCalculator {
   constructor (age, lifeExpectancy, mercuryAge, marsAge, venusAge, jupiterAge, yearsLeft) {
   this.age = age;
   this.lifeExpectancy = lifeExpectancy;
-  this.mercuryAge = mercuryAge;
-  this.venusAge = venusAge;
-  this.jupiterAge = jupiterAge;
-  this.yearsLeft = yearsLeft;
+  this.mercuryAge = .24;
+  this.venusAge = .62;
+  this.marsAge = 1.88;
+  this.jupiterAge = 11.86;
+  this.yearsLeft = 90 - age;
   }
 }
