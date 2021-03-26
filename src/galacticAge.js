cdeb2a180f31b8ca6b23fa4ca12a2f9f8a47e316
Saoud <saoud@saoud.dev>
2021-03-26 14:41:27 -0700

    adding basic constructor to galacticAge.js

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -1,11 +1,15 @@
-export default class AgeCalculator {
+export default class galacticAge {
   constructor (age, lifeExpectancy, mercuryAge, marsAge, venusAge, jupiterAge, yearsLeft) {
   this.age = age;
   this.lifeExpectancy = lifeExpectancy;
-  this.mercuryAge = parseFloat(age * .24).toFixed(1);
-  this.venusAge = parseFloat(age * .62).toFixed(1);
-  this.marsAge = parseFloat(age * 1.88).toFixed(1);
-  this.jupiterAge = parseFloat(age * 11.86).toFixed(1);
-  this.yearsLeft = parseFloat(90 - age).toFixed(1);
+  this.mercuryAge = .24;
+  this.venusAge = .62;
+  this.marsAge = 1.88;
+  this.jupiterAge = 11.86;
+  this.yearsLeft = 90 - age;
   }
-}
+  
+  // getPlanetAge(planet) {
+  //   var planetAge = this.planet * this.age;
+  //   return `Your age on Planet is ${getPlanetAge}
+  // }
