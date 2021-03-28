29a4b96c389a21cbde3734227f38619940734e87
Saoud <saoud@saoud.dev>
2021-03-27 21:39:45 -0700

    adding constructor for passing tests

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -1,15 +1,48 @@
-export default class galacticAge {
-  constructor (age, lifeExpectancy, mercuryAge, marsAge, venusAge, jupiterAge, yearsLeft) {
-  this.age = age;
-  this.lifeExpectancy = lifeExpectancy;
-  this.mercuryAge = .24;
-  this.venusAge = .62;
-  this.marsAge = 1.88;
-  this.jupiterAge = 11.86;
-  this.yearsLeft = 90 - age;
-  }
+export class Age  {
   
-  // getPlanetAge(planet) {
-  //   var planetAge = this.planet * this.age;
-  //   return `Your age on Planet is ${getPlanetAge}
-  // }
+  constructor(earthAge, lifeExp)  {
+    this.earthAge = parseInt(earthAge);
+    this.lifeExp = parseInt(lifeExp);
+    this.yearsLeft = this.yearsDynamic();
+  }
+
+  yearsDynamic() {
+    if (this.earthAge <= this.lifeExp) {
+      return this.yearsLeft = `Your remaining life is about`;
+    } else  {
+      return this.yearsLeft = `You have outlived your life expectancy by`;
+    };
+  }
+
+  mercury()  {
+    this.factor = .24;
+    this.mercuryAge = this.earthAge * this.factor;
+    this.mercuryExp = this.lifeExp * this.factor;
+    this.mercuryAbs = Math.abs(this.mercuryExp - this.mercuryAge);
+    return `Your Mercury stats: Age - ${this.mercuryAge}, Expected life - ${this.mercuryExp}, ${this.yearsLeft} ${this.mercuryAbs} years!`;
+  }
+
+  venus()  {
+    this.factor = .62;
+    this.venusAge = this.earthAge * this.factor;
+    this.venusExp = this.lifeExp * this.factor;
+    this.venusAbs = Math.abs(this.venusExp - this.venusAge);
+    return `Your Venus stats: Age - ${this.venusAge}, Expected life - ${this.venusExp}, ${this.yearsLeft} ${this.venusAbs} years!`;
+  }
+
+  mars()  {
+    this.factor = 1.88
+    this.marsAge = this.earthAge * this.factor;
+    this.marsExp = this.lifeExp * this.factor;
+    this.marsAbs = Math.abs(this.marsExp - this.marsAge);
+    return `Your Mars stats: Age - ${this.marsAge}, Expected life - ${this.marsExp}, ${this.yearsLeft} ${this.marsAbs} years!`;
+  }
+
+  jupiter()  {
+    this.factor = 11.86;
+    this.jupiterAge = Math.round(10 * (this.earthAge * this.factor))/10;
+    this.jupiterExp = Math.round(10 * (this.lifeExp * this.factor))/10;
+    this.jupiterAbs = Math.abs(this.jupiterExp - this.jupiterAge);
+    return `Your Jupiter stats: Age - ${this.jupiterAge}, Expected life - ${this.jupiterExp}, ${this.yearsLeft} ${this.jupiterAbs} years!`;
+  }
+};
